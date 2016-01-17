1be7d487a07fa954ff8ae33cf997b56a3f84c3a0
KLSKO <klaus.kornmeier@axa-groupsolutions.com>
2016-01-17 22:33:16 +0100

    Update Python_training_prework.py

--- Python_training_prework.py
+++ Python_training_prework.py
@@ -1,6 +1,12 @@
-# TO DO: FILEIMPORT masterdata_customer_file.txt
-# up to then: direct input
-masterdata_customer = {"aaa" : "Schmidt", "bbb" : "Dumas", "ddd" : "Daniels"}
+import sys
+import csv
+
+# import csv-file with masterdata
+# and create dictionary
+masterdata_customer = {}
+reader = csv.reader(open("masterdata_customer.csv", "rb"), delimiter=';')
+for row in reader:
+  masterdata_customer.update({row[0] : row[1]})
 
 # Open file with customer IDs
 list_customer_file = open("list_customer.txt").readlines()
@@ -9,7 +15,7 @@ list_customer_file = open("list_customer.txt").readlines()
 list_customer = []
 for i in range(0,len(list_customer_file)):
   list_customer.append(list_customer_file[i].replace("\n",""))
-  
+
 # check if masterdata for customer id already exists
 # if not: prompt for user to enter missing master data
 limit = len(list_customer)
@@ -20,11 +26,13 @@ for i in range(0,limit):
     while confirmation != "y":
       new_masterdata = raw_input("Please enter masterdata for %s: " % list_customer[i] )
       print list_customer[i], new_masterdata
-      confirmation = raw_input("OK? (yes = y, no = n): ")
+      confirmation = raw_input("OK? (y = yes, n = no, q = quit): ")
+      if confirmation == "q":
+        sys.exit(0)
     masterdata_customer[list_customer[i]] = new_masterdata
     masterdata_customer_new[list_customer[i]] = new_masterdata
 
 # TO DO: Export new masterdata file
-# up to then: write to stdout
+# and write to stdout
 print "New masterdata at a glance: "
 masterdata_customer_new
